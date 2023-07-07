db52cd44e0a578b30793943cc4df3a6927da6629
Anna Russkikh <nyusha82@inbox.ru>
2023-07-07 17:41:17 +0300

    Add Tests

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -16,4 +16,60 @@ public class StatisticServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxIfZero() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 0, 3, 8, 6, 11, 11, 12};
+
+
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfNeg() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, -2, 3, 8, 6, 11, 11, 12};
+
+
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfAbout() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 13, 3, 8, 6, 11, 11, 12};
+
+
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfVeryBig() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 123456789, 8, 4, 13, 3, 8, 6, 11, 11, 12};
+
+
+        long expected = 123456789;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
